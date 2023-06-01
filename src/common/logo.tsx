a0c9203cc90ea30328531a65e861d1bb8baf2596
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-06-01 16:40:27 +0530

    fix:ui scaling

--- src/common/logo.tsx
+++ src/common/logo.tsx
@@ -1,6 +1,6 @@
 function Logo() {
   return (
-    <div className="my-5 mx-auto">
+    <div className="my-5 mx-2">
       <div className="border-4 rounded-full flex items-center w-16 h-16 border-logoAccent">
         <div className="border-[3px] rounded-full flex items-center w-[2.8rem] h-[2.8rem] border-logoAccent">
           <div className="border-[2px] rounded-full w-[1.8rem] h-[1.8rem] border-logoAccent"></div>
