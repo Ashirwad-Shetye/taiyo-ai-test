b5759cb825c8afeb385a0fe35453ef4d464a2ecf
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-06-01 16:03:55 +0530

    feat:logo

---
+++ src/common/logo.tsx
@@ -0,0 +1,13 @@
+function Logo() {
+  return (
+    <div className="my-5 mx-auto">
+      <div className="border-4 rounded-full flex items-center w-16 h-16 border-logoAccent">
+        <div className="border-[3px] rounded-full flex items-center w-[2.8rem] h-[2.8rem] border-logoAccent">
+          <div className="border-[2px] rounded-full w-[1.8rem] h-[1.8rem] border-logoAccent"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Logo;
