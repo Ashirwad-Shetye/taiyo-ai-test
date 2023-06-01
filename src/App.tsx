6be224bd02e1f788edc735693363b564ca7a081d
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-06-01 16:57:05 +0530

    feat: Routing for pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import CreateContact from "./pages/createContact";
 import Contacts from "./pages/contacts";
 import ChartsAndMaps from "./pages/chartsAndMaps";
 import SideBar from "./components/sideBar/sideBar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <main className="relative h-[100dvh] flex">
+      <main className="relative h-[100dvh] flex space-x-3">
         <SideBar />
-        <div className="flex-1"></div>
+        <div className="flex-1">
+          <Router>
+            <Routes>
+              <Route path="/" element={<CreateContact />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/charts-and-maps" element={<ChartsAndMaps />} />
+            </Routes>
+          </Router>
+        </div>
       </main>
     </>
   );
