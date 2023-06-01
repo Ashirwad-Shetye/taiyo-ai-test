b9f7c703dc55a107988375acd733b678cc20a4b1
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-06-01 16:55:58 +0530

    fix: sidebar dynamic width

--- src/components/sideBar/sideBar.tsx
+++ src/components/sideBar/sideBar.tsx
@@ -5,11 +5,11 @@ import { LuContact } from "react-icons/lu";
 
 function SideBar() {
   return (
-    <div className="fixed top-0 left-0 h-screen flex w-20 md:w-2/12 ">
-      <div className=" m-3 flex-1 flex flex-col items-center ">
+    <div className="h-screen flex w-20">
+      <div className="bg-white m-3 flex-1 flex flex-col items-center rounded-2xl shadow-xl">
         <Logo />
-        <div className="bg-white flex-1 flex items-center justify-center rounded-2xl shadow-md">
-          <div className="h-full max-h-80 flex flex-col items-center justify-between px-2">
+        <div className="flex-1 flex items-center justify-center">
+          <div className="h-full max-h-80 flex flex-col items-center justify-between">
             <SideBarButton>
               <AiOutlineUserAdd />
             </SideBarButton>
