b1a5d16d4875035646f1b584941b4560c48d8e4f
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-06-01 22:24:14 +0530

    feat: React leaflet map

--- src/components/chartsAndMaps/maps.tsx
+++ src/components/chartsAndMaps/maps.tsx
@@ -1,5 +1,17 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 function Maps() {
-  return <div className="border rounded-lg"></div>;
+  return (
+    <div className="border rounded-lg overflow-hidden">
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+        <TileLayer
+          //   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        ></TileLayer>
+      </MapContainer>
+    </div>
+  );
 }
 
 export default Maps;
