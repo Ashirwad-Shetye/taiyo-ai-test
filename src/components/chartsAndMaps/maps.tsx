b3a78e79ea1d069be75300095152cb97c2de5445
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-06-02 02:22:24 +0530

    feat: react leaflet map

--- src/components/chartsAndMaps/maps.tsx
+++ src/components/chartsAndMaps/maps.tsx
@@ -1,14 +1,31 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { useQuery } from "@tanstack/react-query";
+import { countryCovidData } from "../../services/fetch";
 
 function Maps() {
+  const { data } = useQuery({
+    queryKey: ["countryCovidData"],
+    queryFn: () => countryCovidData,
+  });
+  const marker = data;
+
   return (
     <div className="border rounded-lg overflow-hidden">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-        <TileLayer
-          //   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-        ></TileLayer>
+      <MapContainer center={[20.5937, 78.9629]} zoom={5}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {marker?.map((marker) => (
+          <Marker position={[marker.countryInfo.lat, marker.countryInfo.long]}>
+            <Popup>
+              <div>
+                <h1>{marker.country}</h1>
+                <h2>Active: {marker.active}</h2>
+                <h2>Recovered: {marker.recovered}</h2>
+                <h2>Deaths: {marker.deaths}</h2>
+              </div>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
