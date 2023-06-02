a27895adfc07e3db8e8c8febcc8e8733fccc92ed
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-06-02 09:11:43 +0530

    fix: toggle error

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
@@ -9,7 +11,9 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </QueryClientProvider>
   </React.StrictMode>
 );
--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -4,27 +4,33 @@ import CreateContactForm from "../components/contacts/createContactForm";
 import ContactList from "../components/contacts/contactList";
 
 function Contacts() {
-  const [isCreateContactOpen, setIsCreateContactOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleCreateContactOpen = (value: boolean) => {
-    setIsCreateContactOpen(value);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
-    <div className="h-screen flex">
+    <div className="h-screen relative flex">
       <div className="bg-white m-3 flex flex-col justify-center items-center flex-1 rounded-xl shadow-xl">
         <div className="h-16 w-full pl-5 flex items-center text-left">
           <h1 className="text-2xl font-bold text-gray-700">Contacts</h1>
         </div>
-        {isCreateContactOpen ? (
+        {isModalOpen ? null : (
           <div className="h-24 flex justify-center items-center">
-            <CreateContactButton
-              handleCreateContactOpen={handleCreateContactOpen}
-            />
+            <CreateContactButton openModal={openModal} />
           </div>
-        ) : null}
+        )}
         <div className="flex-1 flex justify-center items-center">
-          {isCreateContactOpen ? <ContactList /> : <CreateContactForm />}
+          {isModalOpen ? (
+            <CreateContactForm closeModal={closeModal} />
+          ) : (
+            <ContactList />
+          )}
         </div>
       </div>
     </div>
