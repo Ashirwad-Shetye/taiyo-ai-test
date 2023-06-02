c49f924e033f9765399d742886896cae23b9216c
Ashirwad P Shetye <99337222+Ashirwad-Shetye@users.noreply.github.com>
2023-06-02 11:12:17 +0530

    chore: render cards

--- src/components/contacts/contactList.tsx
+++ src/components/contacts/contactList.tsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
 import ContactCard from "./contactCard";
+import { getItemFromStorage } from "../../utils/storage";
 
 function ContactList() {
+  const [contacts, setContacts] = useState<[] | null>(null);
+  useEffect(() => {
+    const storedData = getItemFromStorage("contacts");
+
+    if (storedData) {
+      const parsedData = JSON.parse(storedData);
+      setContacts(parsedData);
+    }
+  }, []);
+
   return (
     <div className="flex-1 overflow-hidden">
       <h1 className="text-lg font-semibold text-gray-500 mx-5">Contact List</h1>
@@ -8,12 +20,9 @@ function ContactList() {
         className="overflow-x-hidden justify-items-center mt-5 h-[26rem] grid grid-flow-row-dense md:grid-cols-2 
         lg:grid-cols-3 xl:grid-cols-4 gap-5 overflow-y-scroll"
       >
-        <ContactCard />
-        <ContactCard />
-        <ContactCard />
-        <ContactCard />
-        <ContactCard />
-        <ContactCard />
+        {contacts &&
+          contacts.map((contact) => <ContactCard contact={contact} />)}
+        {!contacts && <h1>Empty</h1>}
       </div>
     </div>
   );
